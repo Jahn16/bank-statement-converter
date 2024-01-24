fd315bcaef946e6a8e112aaba0ea8a550c434ba0
Jahn <jpdeoliveiraferreira@gmail.com>
2024-01-24 08:23:27 -0300

    :recycle: Renamed variable

--- app/entities/csv_writer.py
+++ app/entities/csv_writer.py
@@ -5,10 +5,10 @@ import io
 class CsvWriter:
     @staticmethod
     def write(transactions: list[dict[str, str]]) -> io.StringIO:
-        output = io.StringIO()
+        data = io.StringIO()
         field_names = transactions[0].keys()
-        writer = csv.DictWriter(output, field_names, delimiter=";")
+        writer = csv.DictWriter(data, field_names, delimiter=";")
         writer.writeheader()
         writer.writerows(transactions)
 
-        return output
+        return data
