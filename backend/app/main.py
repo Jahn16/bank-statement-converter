0c22da99bf445ab5194d856184c2ac7b67ced04b
Jahn <jpdeoliveiraferreira@gmail.com>
2024-01-25 21:50:16 -0300

    :adhesive_bandage: Enabled CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import convert
 
@@ -6,6 +7,18 @@ app = FastAPI(
     title="Fatura2Csv",
     version="0.1.0",
     description="Aplicação para converter faturas em PDF para CSV",
+)
+
+
+# TODO: make a setting class to store origins
+origins = ["http://localhost:3000", "localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(convert.router)
